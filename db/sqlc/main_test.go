1574af8aabd1c8a22866f4eb0e6c1807405a52c5
Pablo Pelardas <pablo.pelardas@gmail.com>
2023-07-08 02:01:06 -0300

    store for transactions and unit test

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -9,17 +9,21 @@ import (
 	_ "github.com/lib/pq"
 )
 
-var testQueries *Queries
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:pablo2846@localhost:5432/gopg-db-test?sslmode=disable"
+)
 
-var dbDriver = "postgres"
-var dbSource = "postgresql://root:pablo2846@localhost:5432/gopg-db-test?sslmode=disable"
+var testQueries *Queries
+var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	conn, err := sql.Open(dbDriver, dbSource)
+	var err error
+	testDB, err = sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
-	testQueries = New(conn)
+	testQueries = New(testDB)
 
 	os.Exit(m.Run())
 }
